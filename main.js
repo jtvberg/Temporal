9fadc1db034bdff917c4ef537067b337519dfc9a
jtvberg <jtvberg@gmail.com>
2020-08-18 15:21:03 -0500

    basic note with persistence

--- main.js
+++ main.js
@@ -1,18 +1,17 @@
 const {app, BrowserWindow} = require('electron')
-const path = require('path')
 
 function createWindow () {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
-    // webPreferences: {
-    //   preload: path.join(__dirname, 'preload.js')
-    // }
+    webPreferences: {
+      nodeIntegration: true
+    }
   })
 
   mainWindow.loadFile('main.html')
 
-  // Open the DevTools
+  // Open DevTools
   mainWindow.webContents.openDevTools()
 }
 
--- renderer.js
+++ renderer.js
@@ -1,14 +1,15 @@
+const $ = require('jquery')
 let change = false
-var note = document.getElementsByClassName('note')[0]
-note.innerHTML = localStorage.getItem('note')
+$('.note')[0].innerHTML = localStorage.getItem('note')
 var noteMo = new window.MutationObserver(function(e) {
     change = true
 })
-noteMo.observe(note, { childList: true, subtree: true, characterData: true })
-const noteBlur = () => {
+noteMo.observe($('.note')[0], { childList: true, subtree: true, characterData: true })
+
+$('.note').blur( () => {
   if (change) {
-    console.log(note.innerHTML)
-    localStorage.setItem('note', note.innerHTML)
+    console.log($('.note')[0].innerHTML)
+    localStorage.setItem('note', $('.note')[0].innerHTML)
     change = false
   }
-}
+})
