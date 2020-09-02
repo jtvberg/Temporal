a52f23f9512d0605a5157c15fd4037a559684583
Vandenberg, Joel <joel.vandenberg@optum.com>
2020-09-02 18:59:11 -0500

    Comments and clean-up

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
+// Imports and variable declarations
 const { app, BrowserWindow, ipcMain, Tray } = require('electron')
 
 // Enable Electron-Reload (dev only)
 require('electron-reload')(__dirname)
 
+// Main window
 let win = null
 const createWindow = () => {
   win = new BrowserWindow({
@@ -20,18 +22,22 @@ const createWindow = () => {
     }
   })
 
+  // HTML file to load into window
   win.loadFile('main.html')
 
+  // Show window when ready
   win.once('ready-to-show', () => {
     win.show()
   })
 
+  // Set initial state of always on top
   win.setAlwaysOnTop(true, 'floating')
 
   // Open DevTools (dev only)
   win.webContents.openDevTools()
 }
 
+// Tray icon
 let tray = null
 const createTray = () => {
   tray = new Tray('icon.png')
@@ -41,8 +47,10 @@ const createTray = () => {
   })
 }
 
+// Remove app from dock
 app.dock.hide()
 
+// Instantiate window and tray on app ready
 app.whenReady().then(() => {
   createWindow()
   createTray()
@@ -51,22 +59,27 @@ app.whenReady().then(() => {
   })
 })
 
+// CLose app if all windows are closed (not Mac)
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') app.quit()
 })
 
+// IPC channel for locking app on top
 ipcMain.on('ontop-lock', function () {
   win.setAlwaysOnTop(true, 'floating')
 })
 
+// IPC channel for unlocking app on top
 ipcMain.on('ontop-unlock', function () {
   win.setAlwaysOnTop(false)
 })
 
+// IPC channel for maximizing window
 ipcMain.on('win-max', () => {
   win.maximize()
 })
 
+// IPC channel for restoring window
 ipcMain.on('win-restore', () => {
   win.unmaximize()
 })
--- renderer.js
+++ renderer.js
@@ -1,34 +1,21 @@
+// Imports and variable declarations
 const { ipcRenderer, clipboard } = require('electron')
 const $ = require('jquery')
 let change = false
 let winMax = false
 
-$('#note-button-0').addClass('note-button-selected')
-$('#note-0').show()
-$('#sketch-0').show()
+// Load methods
+setFirstNote()
+changeWatch($('.note-host'))
 
-$('.note').each(function () {
-  $(this)[0].innerHTML = JSON.parse(localStorage.getItem('notes')) ? JSON.parse(localStorage.getItem('notes'))[$(this).data('val')] : ''
-})
+// Select first note
+function setFirstNote () {
+  $('#note-button-0').addClass('note-button-selected')
+  $('#note-0').show()
+  $('#sketch-0').show()
+}
 
-$('.note').blur(() => {
-  if (change) {
-    const notes = []
-    $('.note').each(function () {
-      notes.push($(this)[0].innerHTML)
-    })
-    localStorage.setItem('notes', JSON.stringify(notes))
-    change = false
-  }
-})
-
-$('.note-button').click((e) => {
-  $('.note-button').removeClass('note-button-selected')
-  $('.note, .sketch').hide(0)
-  $(`#note-${$(e.currentTarget).data('val')}, #sketch-${$(e.currentTarget).data('val')}`).show()
-  $(`#note-button-${$(e.currentTarget).data('val')}`).addClass('note-button-selected')
-})
-
+// Mutation observer to track note changes
 function changeWatch (note) {
   var observer = new window.MutationObserver(function (e) {
     change = true
@@ -36,36 +23,7 @@ function changeWatch (note) {
   observer.observe(note[0], { childList: true, subtree: true, characterData: true })
 }
 
-changeWatch($('.note-host'))
-
-$('.ontop-button').click(function () {
-  if ($(this).hasClass('ontop-locked')) {
-    $(this).removeClass('ontop-locked').addClass('ontop-unlocked')
-    ipcRenderer.send('ontop-unlock')
-  } else {
-    $(this).removeClass('ontop-unlocked').addClass('ontop-locked')
-    ipcRenderer.send('ontop-lock')
-  }
-})
-
-$('.sketch-button').click(function () {
-  if ($(this).hasClass('sketch-mode')) {
-    $(this).removeClass('sketch-mode')
-    $('.note').each(function () {
-      $(this).css('z-index', '20')
-    })
-  } else {
-    $(this).addClass('sketch-mode')
-    $('.note').each(function () {
-      $(this).css('z-index', '5')
-    })
-  }
-})
-
-$('.header-bar').dblclick(() => {
-  maxRestoreWindow()
-})
-
+// Window max/restore on header double click
 function maxRestoreWindow () {
   if (!winMax) {
     ipcRenderer.send('win-max')
@@ -78,6 +36,7 @@ function maxRestoreWindow () {
   }
 }
 
+// Create canvases for sketch mode
 function sketchCanvas (canvasElement, strokeColor) {
   const canvas = canvasElement[0]
   const ctx = canvas.getContext('2d')
@@ -112,7 +71,6 @@ function sketchCanvas (canvasElement, strokeColor) {
   })
 
   $(canvasElement).contextmenu(() => {
-    console.log('right')
     ctx.clearRect(0, 0, canvas.width, canvas.height)
   })
 
@@ -134,10 +92,68 @@ function sketchCanvas (canvasElement, strokeColor) {
   }, false)
 }
 
+// Get note content from local storage
+$('.note').each(function () {
+  $(this)[0].innerHTML = JSON.parse(localStorage.getItem('notes')) ? JSON.parse(localStorage.getItem('notes'))[$(this).data('val')] : ''
+})
+
+// Save note content to local storage
+$('.note').blur(() => {
+  if (change) {
+    const notes = []
+    $('.note').each(function () {
+      notes.push($(this)[0].innerHTML)
+    })
+    localStorage.setItem('notes', JSON.stringify(notes))
+    change = false
+  }
+})
+
+// Toggle to selected note/sketch pair
+$('.note-button').click((e) => {
+  $('.note-button').removeClass('note-button-selected')
+  $('.note, .sketch').hide(0)
+  $(`#note-${$(e.currentTarget).data('val')}, #sketch-${$(e.currentTarget).data('val')}`).show()
+  $(`#note-button-${$(e.currentTarget).data('val')}`).addClass('note-button-selected')
+})
+
+// Toggle keep on top
+$('.ontop-button').click(function () {
+  if ($(this).hasClass('ontop-locked')) {
+    $(this).removeClass('ontop-locked').addClass('ontop-unlocked')
+    ipcRenderer.send('ontop-unlock')
+  } else {
+    $(this).removeClass('ontop-unlocked').addClass('ontop-locked')
+    ipcRenderer.send('ontop-lock')
+  }
+})
+
+// Toggle between note and sketch canvas
+$('.sketch-button').click(function () {
+  if ($(this).hasClass('sketch-mode')) {
+    $(this).removeClass('sketch-mode')
+    $('.note').each(function () {
+      $(this).css('z-index', '20')
+    })
+  } else {
+    $(this).addClass('sketch-mode')
+    $('.note').each(function () {
+      $(this).css('z-index', '5')
+    })
+  }
+})
+
+// Header double-click handler
+$('.header-bar').dblclick(() => {
+  maxRestoreWindow()
+})
+
+// Iterate and instantiate sketch canvases
 $('.sketch').each(function () {
   sketchCanvas($(this), $(this).css('color'))
 })
 
+// Capture paste to note and push text only
 $('.note').on('paste', function (e) {
   e.preventDefault()
   const caretPos = $(this)[0].selectionStart
